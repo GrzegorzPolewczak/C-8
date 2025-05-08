8c6d77eb358f61196b2c741dc3d671bc63cb7758
GrzegorzPolewczak <148885875+GrzegorzPolewczak@users.noreply.github.com>
2025-05-08 12:41:26 +0200

    Add files via upload

---
+++ ZadanieBLab8/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZadanieBLab8.Model;
+
+namespace ZadanieBLab8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWczytaj_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new DbUczelnia())
+            {
+
+                foreach(var student in db.Studenci.OrderBy(s=>s.Nazwisko))
+                {
+                    lbxListaStudentow.Items.Add(student);
+                }
+
+                var sredniaOcen = db.Studenci.Average(s=>s.Ocena);
+                lbSredniaOcen.Content = $"Średnia ocen: {sredniaOcen:F2}";
+            }
+        }
+    }
+}
